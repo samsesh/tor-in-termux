9b68c4f4af3b011e15883c06c1fbab2a717f924f
127.0.0.1 <22117518+samsesh@users.noreply.github.com>
2019-07-02 23:12:28 +0430

    Update run.sh

--- run.sh
+++ run.sh
@@ -86,13 +86,15 @@ sleep 1
 clear
 apt update
 apt upgrade -y
-apt install figlet 
+apt install figlet
+clear 
 figlet -f banner wait
 sleep 3
 figlet -f digital start
 echo "installing...."
 sleep 2
 apt install tor zsh wget git -y
+clear
 figlet wait 
 sleep 3
 ##sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
@@ -132,9 +134,10 @@ echo figlet -f big run TOR >> .zshrc
 echo sleep 5 >> .zshrc
 echo tor --HTTPTunnelPort 8118 >> .zshrc
 ‎‏echo exit >> .zshrc
+clear
 figlet -f big DONE
 echo -e "$blue set socks5 proxy in telegram 127.0.0.1:9050"
 echo -e "$green set http proxy in twitter 127.0.0.1:8118"
-echo -e "$red type zsh then close termux and run again"
+echo -e "$red type zsh then close termux and run again $Cafe"
 sleep 3
 chsh
